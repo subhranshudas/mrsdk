5ee48e1dda60504c3ef3b1df2a1c56afe12f949c
Subhranshu Das <subhranshu@Subhranshus-MacBook-Pro.local>
2022-04-29 17:09:53 +0530

    fix(utils): modified console

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,5 +1,5 @@
 require('dotenv').config({ path: '../../.env' });
-console.log("@mrsdk-utils: calling code from packages/utils");
+console.log("@mrsdk-utils: calling code from packages/utils: edited");
 
 export const greet = () => {
     console.log(`Utils.greet() says Hi from ${process.env.BLOCKCHAIN_NETWORK} !`);
