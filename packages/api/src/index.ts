e50eebab39ba35c0acf4ef7b9cd8f2fa134d3ad5
Subhranshu Das <subhranshu@Subhranshus-MacBook-Pro.local>
2022-04-29 17:12:24 +0530

    fix(utils): changed msg fix(api): changed log

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,6 +4,6 @@ import * as Utils from '@dasubh/mrsdk-utils';
 console.log("@mrsdk-api: calling code from packages/api");
 
 const flag = process.env.BLOCKCHAIN_NETWORK === 'KOVAN' ? 'you get free ETH' : 'you have to pay!';
-console.log(`Ok so, ${flag}`);
+console.log(`Ok so now..., ${flag}`);
 
 Utils.greet();
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,6 @@
 require('dotenv').config({ path: '../../.env' });
-console.log("@mrsdk-utils: calling code from packages/utils: edited");
+console.log("@mrsdk-utils: calling code from packages/utils");
 
 export const greet = () => {
-    console.log(`Utils.greet() says Hi from ${process.env.BLOCKCHAIN_NETWORK} !`);
+    console.log(`Utils.greet() says Hello! from ${process.env.BLOCKCHAIN_NETWORK} !`);
 };
